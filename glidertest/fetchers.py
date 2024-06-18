9871bd3ab21b4e639e80d4e52e2b38575a86ed52
Callum Rollo <c.rollo@outlook.com>
2024-06-18 15:25:00 +0200

    added new example file with backscatter, adcp pres and more

--- glidertest/fetchers.py
+++ glidertest/fetchers.py
@@ -7,7 +7,7 @@ data_source_og = pooch.create(
     base_url=server,
     registry={
         "sea076_20230906T0852_R.nc": "sha256:19e55e7018dc578a58a184b9d03e403e18470f59d20038deff59ab962c737902",
-        "sea076_20230906T0852_delayed.nc": "sha256:bd3d87d2ae476358769cb030dbf9478a4fc9f70fcf752a1bdd8c68f3af5f93ef",
+        "sea076_20230906T0852_delayed.nc": "sha256:bd50b2c1019b49f8c14381de8f78aa67d66e9d9e89607bbcedff246be60e6c92",
     },
 )
 
