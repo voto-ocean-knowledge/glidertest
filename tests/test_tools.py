e23de1ffcd1a6d2de33914a7bcbbec0a3284cafa
Chiara Monforte <monforte.chiara.mc@gmail.com>
2024-11-11 14:51:08 +0100

    Fixed issues (not calling figures etc.)

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -6,62 +6,60 @@ import math
 
 def test_plots():
     ds = fetchers.load_sample_dataset()
-    tools.plot_basic_vars(ds,start_prof=0, end_prof=int(ds.PROFILE_NUMBER.max()))
+    fig, ax = tools.plot_basic_vars(ds,start_prof=0, end_prof=int(ds.PROFILE_NUMBER.max()))
     assert ax[0].get_ylabel() == 'Depth (m)'
     assert ax[0].get_xlabel() == f'Average Temperature [C] \nbetween profile {start_prof} and {end_prof}'
     return fig
 
 
-def test_up_down_bias():
+def test_up_down_bias(v_res=1, xlabel='Salinity'):
     ds = fetchers.load_sample_dataset()
     fig, ax = plt.subplots()
-    df = tools.updown_bias(ds, var='PSAL', v_res=1)
+    df = tools.updown_bias(ds, var='PSAL', v_res=v_res)
     bins = np.unique(np.round(ds.DEPTH,0))
     ncell = math.ceil(len(bins)/v_res)
     assert len(df) == ncell
-    tools.plot_updown_bias(df, ax,  xlabel='Salinity')
+    tools.plot_updown_bias(df, ax,  xlabel=xlabel)
     lims = np.abs(df.dc)
     assert ax.get_xlim() == (-np.nanpercentile(lims, 99.5), np.nanpercentile(lims, 99.5))
     assert ax.get_ylim() == (df.depth.max() + 1, -df.depth.max() / 30)
-    assert ax[0].get_xlabel() == xlabel
+    assert ax.get_xlabel() == xlabel
  
 
-def test_chl():
+def test_chl(var1='CHLA', var2='BBP700'):
     ds = fetchers.load_sample_dataset()
-    tools.optics_first_check(ds, var='CHLA')
-    assert ax.get_ylabel() == var
-    tools.optics_first_check(ds, var='BBP700')
-    assert ax.get_ylabel() == var
+    ax = tools.optics_first_check(ds, var=var1)
+    assert ax.get_ylabel() == var1
+    ax = tools.optics_first_check(ds, var=var2)
+    assert ax.get_ylabel() == var2
     with pytest.raises(KeyError) as e:
         tools.optics_first_check(ds, var='nonexistent_variable')
 
 
-def test_quench_sequence():
+def test_quench_sequence(xlabel='Temperature [C]',ylim=45):
     ds = fetchers.load_sample_dataset()
     if not "TIME" in ds.indexes.keys():
         ds = ds.set_xindex('TIME')
     fig, ax = plt.subplots()
-    tools.plot_section_with_srss(ds, 'CHLA')
-    assert ax.get_ylabel == 'Depth [m]'
+    tools.plot_section_with_srss(ds, 'CHLA', ax,ylim=ylim)
+    assert ax.get_ylabel() == 'Depth [m]'
     assert ax.get_ylim() == (ylim, -ylim / 30)
     
     dayT, nightT = tools.day_night_avg(ds, sel_var='TEMP')
     assert len(nightT.dat.dropna()) > 0
     assert len(dayT.dat.dropna()) > 0
     
-    fig, ax = plt.subplots()
-    tools.plot_daynight_avg(dayT, nightT, ax, xlabel='Temperature [C]') 
-    assert ax.get_ylabel() == 'Depth (m)'
+    fig, ax = tools.plot_daynight_avg(dayT, nightT,xlabel=xlabel) 
+    assert ax.get_ylabel() == 'Depth [m]'
     assert ax.get_xlabel() == xlabel
-    assert ax.get_title() == sel_day
 
-def test_temporal_drift():
+def test_temporal_drift(var='DOXY'):
     ds = fetchers.load_sample_dataset()
     fig, ax = plt.subplots(1, 2)
-    tools.check_temporal_drift(ds,'DOXY', ax)
-    assert ax.get_ylabel() == 'Depth (m)'
-    assert ax.get_xlabel() == var
-    assert ax.get_xlim == (np.nanpercentile(ds[var], 0.01), np.nanpercentile(ds[var], 99.99))
+    tools.check_temporal_drift(ds,var, ax)
+    assert ax[1].get_ylabel() == 'Depth (m)'
+    assert ax[0].get_ylabel() == var
+    assert ax[1].get_xlim() == (np.nanpercentile(ds[var], 0.01), np.nanpercentile(ds[var], 99.99))
     tools.check_temporal_drift(ds,'CHLA')
         
 def test_profile_check():
